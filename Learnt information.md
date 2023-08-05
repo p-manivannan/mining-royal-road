94b6346a025c7bcbd550bb0f9ff95c9bec50a2bf
countnye <91778509+countnye@users.noreply.github.com>
2023-08-05 13:21:21 +0200

    Proof of concept ability to fetch reviews

---
+++ Learnt information.md
@@ -0,0 +1,3 @@
+# What I learnt
+- Xpath: It is a format used to describe the location of any element on a webpage. Note that the actual HTML isn't coded in the Xpath.
+- Python's request library can't handle dynamic webpages. This is why Selenium is used
--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 from selenium import webdriver
+import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-# Uncomment if Chrome driver does not exist, or is installed in the wrong path
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# Download the correct chromedriver (if using chrome. For other browsers, use relevant driver) and place in path
+# driver = webdriver.Chrome(r"C:\Users\pithw\Desktop\University\1_PERSONAL PROJECTS\chromedriver")
 
 novel_link = 'https://www.royalroad.com/fiction/21220/mother-of-learning'
 
-driver.get(novel_link)
-content = driver.page_source
-soup = BeautifulSoup(content)
-print(soup)
+# driver.get(novel_link)
+# for review_element in driver.find_elements_by_class_name("review-inner"):
+#     print(review_element)
 
-# Returns some install error
+page = requests.get(novel_link).text
+soup = BeautifulSoup(page, features='lxml')
+results = soup.find_all('div', class_='review-inner')
+reviews = {}
+reviews['review'] = []
+
+for i in results:
+    reviews['review'].append(i.text)
+
+
+
+
+'''
+BeautifulSoup can't access reviews just be getting the driver 
+page source without modifications.
+
+There's something in the review text in the html called '&nbsp;'.
+It most likely functions like <br> in the text
+'''
+
+
+
 
