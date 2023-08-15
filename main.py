81f8a0078bb9ff3ba6be5750e63ab5547ea05868
countnye <91778509+countnye@users.noreply.github.com>
2023-08-15 15:42:56 +0200

    idk what i updated actually

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from functions import *
 
 # nlp = spacy.load("en_core_web_lg")
 
+'''
+TO-DO:
+1. Get summary
+2. Get ALL reviews
+3. Convert novel_collection to pandas dataframe
+'''
 
 novel_link = 'https://www.royalroad.com/fiction/21220/mother-of-learning'
 
@@ -28,16 +34,8 @@ novel_collection = {
                     }
 
 print(novel_collection)
-# # Objective 1: Put reviews in a pandas dataframe
-# # Objective 2: Pre-process text
-# # Objective 3: Get ALL reviews, not just the ones on the first page
 
-
-
-# for i in results:
-#     reviews.append(i.text)
-
-# print(reviews)
+# Getting all reviews
 
 
 '''
